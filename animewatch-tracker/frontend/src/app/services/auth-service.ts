8db5a41de7ec58c31c7a59a1be868515f27abedf
Alejandro Plata <alejandroplatacortes@gmail.com>
2025-12-14 21:01:09 +0100

    .

--- animewatch-tracker/frontend/src/app/services/auth-service.ts
+++ animewatch-tracker/frontend/src/app/services/auth-service.ts
@@ -1,37 +1,27 @@
 import { Injectable } from '@angular/core';
 import { User, UserFavorites } from '../types/user';
 import { Favorite } from '../types/user';
-
 @Injectable({
   providedIn: 'root',
 })
-
-/* No he protegido las rutas del dashboard (en el backend si) porque no sabía como en angular y no me daba tiempo a investigarlo */
-
 export class AuthService {
-
   API_URL = 'https://backend-anime.vercel.app/api';
   STORAGE_KEY = 'user';
   TOKEN_KEY = 'auth_token';
-
   async login(username: string, password: string): Promise<User> {
     const response = await fetch(`${this.API_URL}/auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password }),
     });
-
     if (!response.ok) {
       const errorData = await response.json();
       throw new Error(errorData.error || 'Error al iniciar sesión');
     }
-
     const data = await response.json();
-
     if (data.token) {
       localStorage.setItem(this.TOKEN_KEY, data.token);
     }
-
     const token = localStorage.getItem(this.TOKEN_KEY);
     const userResponse = await fetch(`${this.API_URL}/auth/user`, {
       method: 'GET',
@@ -40,35 +30,27 @@ export class AuthService {
         'Authorization': `Bearer ${token}`
       },
     });
-
     if (!userResponse.ok) {
       throw new Error('Error al obtener datos del usuario');
     }
-
     const user: User = await userResponse.json();
     localStorage.setItem(this.STORAGE_KEY, JSON.stringify(user));
-
     return user;
   }
-
   async register(data: { username: string; email: string; password: string }): Promise<User> {
     const response = await fetch(`${this.API_URL}/auth/register`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
     });
-
     if (!response.ok) {
       const errorData = await response.json();
       throw new Error(errorData.error || 'Error al registrar');
     }
-
     const responseData = await response.json();
-
     if (responseData.token) {
       localStorage.setItem(this.TOKEN_KEY, responseData.token);
     }
-
     const token = localStorage.getItem(this.TOKEN_KEY);
     const userResponse = await fetch(`${this.API_URL}/auth/user`, {
       method: 'GET',
@@ -77,17 +59,13 @@ export class AuthService {
         'Authorization': `Bearer ${token}`
       },
     });
-
     if (!userResponse.ok) {
       throw new Error('Error al obtener datos del usuario');
     }
-
     const user: User = await userResponse.json();
     localStorage.setItem(this.STORAGE_KEY, JSON.stringify(user));
-
     return user;
   }
-
   async logout(): Promise<void> {
     try {
       const token = localStorage.getItem(this.TOKEN_KEY);
@@ -105,7 +83,6 @@ export class AuthService {
       localStorage.removeItem(this.TOKEN_KEY);
     }
   }
-
   getCurrentUser(): UserFavorites | null {
     try {
       const stored = localStorage.getItem(this.STORAGE_KEY);
@@ -116,7 +93,6 @@ export class AuthService {
       return null;
     }
   }
-
   async refreshUser(): Promise<void> {
     try {
       const token = localStorage.getItem(this.TOKEN_KEY);
@@ -135,52 +111,38 @@ export class AuthService {
       console.log(error);
     }
   }
-
   async getIsFavorite(animeId: number): Promise<boolean> {
     await this.refreshUser();
     const user = this.getCurrentUser();
-
     if (!user || !user.favorites) {
       return false;
     }
-
     return user.favorites.some(fav => fav.animeId === animeId);
   }
-
   async getFavoriteState(animeId: number): Promise<string | null> {
     await this.refreshUser();
     const user = this.getCurrentUser();
-
     if (!user || !user.favorites) {
       return null;
     }
-
     const favorite = user.favorites.find(fav => fav.animeId === animeId);
     return favorite ? favorite.state : null;
   }
-
   async getFavoriteScore(animeId: number): Promise<number | null> {
-
     await this.refreshUser();
-
     const user = this.getCurrentUser();
-
     if (!user || !user.favorites) {
       return null;
     }
-
     const favorite = user.favorites.find(fav => fav.animeId === animeId);
     return favorite ? favorite.score : null;
   }
-
   async getFavorites(): Promise<Favorite[]> {
     await this.refreshUser();
-
     const user = this.getCurrentUser();
     if (!user || !user.favorites) return [];
     return user.favorites;
   }
-
   async addAnimeToFavorites(animeId: number, state: string = 'VIENDO'): Promise<void> {
     const token = localStorage.getItem(this.TOKEN_KEY);
     const response = await fetch(`${this.API_URL}/user/favorites`, {
@@ -191,15 +153,12 @@ export class AuthService {
       },
       body: JSON.stringify({ animeId, state }),
     });
-
     if (!response.ok) {
       const errorData = await response.json();
       throw new Error(errorData.msg || 'Error al añadir a favoritos');
     }
-
     await this.refreshUser();
   }
-
   async removeAnimeFromFavorites(animeId: number): Promise<void> {
     const token = localStorage.getItem(this.TOKEN_KEY);
     const response = await fetch(`${this.API_URL}/user/favorites/${animeId}`, {
@@ -209,15 +168,12 @@ export class AuthService {
         'Authorization': `Bearer ${token}`
       },
     });
-
     if (!response.ok) {
       const errorData = await response.json();
       throw new Error(errorData.msg || 'Error al eliminar de favoritos');
     }
-
     await this.refreshUser();
   }
-
   async patchFavoriteState(animeId: number, state: string): Promise<void> {
     const token = localStorage.getItem(this.TOKEN_KEY);
     const response = await fetch(`${this.API_URL}/user/favorites/${animeId}/state`, {
@@ -228,15 +184,12 @@ export class AuthService {
       },
       body: JSON.stringify({ state }),
     });
-
     if (!response.ok) {
       const errorData = await response.json();
       throw new Error(errorData.msg);
     }
-
     await this.refreshUser();
   }
-
   async patchFavoriteScore(animeId: number, score: number): Promise<void> {
     const token = localStorage.getItem(this.TOKEN_KEY);
     const response = await fetch(`${this.API_URL}/user/favorites/${animeId}/puntuation`, {
@@ -247,12 +200,10 @@ export class AuthService {
       },
       body: JSON.stringify({ puntuation: score }),
     });
-
     if (!response.ok) {
       const errorData = await response.json();
       throw new Error(errorData.msg);
     }
-
     await this.refreshUser();
   }
 }
